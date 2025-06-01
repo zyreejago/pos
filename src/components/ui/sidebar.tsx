8b18156f65bf0944b1c4a7880d506838bb9e7997
Meyka Pradnya <meykapradnya04@gmail.com>
2025-06-01 06:49:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: `DialogContent` requires a `DialogTitle` for the component to be accessible for screen reader users.
    >
    > If you want to hide the `DialogTitle`, you can wrap it with our VisuallyHidden component.
    >
    > For more information, see https://radix-ui.com/primitives/docs/components/dialog. Error source: src/components/ui/sheet.tsx (62:5) @ _c1
    >
    >   60 |   <SheetPortal>
    >   61 |     <SheetOverlay />
    > > 62 |     <SheetPrimitive.Content
    >      |     ^
    >   63 |       ref={ref}
    >   64 |       className={cn(sheetVariants({ side }), className)}
    >   65 |       {...props}
    >
    > Call Stack
    > 29
    >
    > Show 24 ignore-listed frame(s)
    > _c1
    > src/components/ui/sheet.tsx (62:5)
    > _c1
    > src/components/ui/sheet.tsx (60:3)
    > Sidebar
    > src/components/ui/sidebar.tsx (198:11)
    > AppSidebar
    > src/components/layout/app-sidebar.tsx (75:5)
    > AppLayout
    > src/app/(app)/layout.tsx (32:11)

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -10,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -206,6 +207,7 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <SheetTitle className="sr-only">Mobile Navigation Menu</SheetTitle>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
