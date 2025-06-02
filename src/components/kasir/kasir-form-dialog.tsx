153782bbdda46ec41d310df29d91e8182ac2f594
Meyka Pradnya <meykapradnya04@gmail.com>
2025-06-02 03:11:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: React is not defined. Error source: src/components/kasir/kasir-form-dialog.tsx (132:13) @ KasirFormDialog
    >
    >   130 |       <DialogTrigger asChild>
    >   131 |         {triggerButton ? (
    > > 132 |             React.cloneElement(triggerButton as React.ReactElement<any>, { onClick: handleDialogTriggerClick })
    >       |             ^
    >   133 |         ) : (
    >   134 |           <Button onClick={handleDialogTriggerClick} disabled={allOutlets.length === 0 && !kasir}>
    >   135 |             <PlusCircle className="mr-2 h-4 w-4" /> Add Kasir
    >
    > Call Stack
    > 3
    >
    > KasirFormDialog
    > src/components/kasir/kasir-form-dialog.tsx (132:13)
    > KasirTable
    > src/components/kasir/kasir-table.tsx (373:9)
    > ManageKasirPage
    > src/app/(app)/kasir/page.tsx (22:11)

--- src/components/kasir/kasir-form-dialog.tsx
+++ src/components/kasir/kasir-form-dialog.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react"; // Import React
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
